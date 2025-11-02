d6a40b70b64f22224cced4dc9f27e1643a85639e
Hemanth0411 <hemanthreddyannem@gmail.com>
2025-11-02 12:25:11 +0530

    Created initial project structure

---
+++ nirman/builder.py
@@ -0,0 +1,6 @@
+def build_structure(tree, output_path, dry_run=False, force=False):
+    """
+    Builds the directory and file structure from a parsed tree.
+    (To be implemented in a future issue)
+    """
+    pass
